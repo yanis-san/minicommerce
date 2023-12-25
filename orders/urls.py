c34abe5fc1ebd7ccbe7fb633bf12e32f80db2a34
yanis-san <barbarayanis14@gmail.com>
2023-12-25 11:16:11 +0100

    No online payment version of the website

---
+++ orders/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from orders.views import order_create, admin_order_detail
+
+app_name = 'orders'
+urlpatterns = [
+    path('create/', order_create, name='order_create'),
+    path('admin/order/<int:order_id>/', admin_order_detail, name='admin_order_detail'),
+]
---
+++ orders/views.py
@@ -0,0 +1,36 @@
+from django.urls import reverse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.admin.views.decorators import staff_member_required
+from .models import OrderItem, Order
+from .forms import OrderCreateForm
+
+from cart.cart import Cart
+
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order,
+                                         product=item['product'],
+                                         price=item['price'],
+                                         quantity=item['quantity'])
+            # clear the cart
+            cart.clear()
+            return render(request,
+                          'orders/order/created.html',
+                          {'order': order})
+    else:
+        form = OrderCreateForm()
+    return render(request,
+                  'orders/order/create.html',
+                  {'cart': cart, 'form': form})
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'admin/orders/order/detail.html', {'order': order})
